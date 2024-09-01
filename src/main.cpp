58a9e6b0f2313ae8cb0deb5ae7975bac17f54642
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-09-01 23:39:21 +0200

    Removed vertex class will restructure first

--- src/main.cpp
+++ src/main.cpp
@@ -10,46 +10,6 @@
 const uint32_t WIDTH = 1280;
 const uint32_t HEIGHT = 720;
 
-struct Vertex {
-    glm::vec2 pos;
-    glm::vec3 color;
-
-    static VkVertexInputBindingDescription getBindingDescription() {
-        VkVertexInputBindingDescription bindingDescription{};
-        bindingDescription.binding = 0;
-        bindingDescription.stride = sizeof(Vertex);
-        bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
-
-        return bindingDescription;
-    }
-
-    static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
-        std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
-
-        attributeDescriptions[0].binding = 0;
-        attributeDescriptions[0].location = 0;
-        attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
-        attributeDescriptions[0].offset = offsetof(Vertex, pos);
-
-        attributeDescriptions[1].binding = 0;
-        attributeDescriptions[1].location = 1;
-        attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
-        attributeDescriptions[1].offset = offsetof(Vertex, color);
-
-        return attributeDescriptions;
-    }
-};
-
-std::vector<Vertex> vertices = {
-    {{-0.5f, -0.5f}, {1.0f, 0.0f, 1.0f}}, // Bottom-left vertex, pink color
-    {{0.5f, -0.5f}, {1.0f, 0.0f, 1.0f}},  // Bottom-right vertex, pink color
-    {{0.5f, 0.5f}, {1.0f, 0.0f, 1.0f}},   // Top-right vertex, pink color
-    {{-0.5f, 0.5f}, {1.0f, 0.0f, 1.0f}}   // Top-left vertex, pink color
-};
-
-std::vector<uint16_t> indices = {
-    0, 1, 2, 2, 3, 0};
-
 class VulkanEngine {
 public:
     void run() {
