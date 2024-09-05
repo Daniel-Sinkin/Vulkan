467a199728037a538f7a8db1c78cad334ab6257a
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-09-05 06:20:00 +0200

    Added validation layers

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,15 @@
 const uint32_t WINDOW_WIDTH = 800;
 const uint32_t WINDOW_HEIGHT = 600;
 
+const std::vector<const char *> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
+
 class HelloTriangleApplication {
 public:
     void run() {
