0d707546c274021a661c8c23aaec5f86b407c638
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-09-06 10:00:01 +0200

    More cleanup

--- clean_and_run.sh
+++ clean_and_run.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-# Clean and create build directory
 rm -rf build
 mkdir build
 cd build
 
-# Check if the --release argument is passed
 BUILD_TYPE="Debug"
 if [[ "$1" == "--release" ]]; then
     BUILD_TYPE="Release"
 fi
 
-# Configure the project with the appropriate build type
 cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
 
-# Determine the number of CPU cores, fallback to 1 if sysctl is not available
 if command -v sysctl &> /dev/null; then
     NUM_CORES=$(sysctl -n hw.ncpu)
     NUM_CORES=$((NUM_CORES - 1))
@@ -24,8 +20,6 @@ else
     NUM_CORES=1
 fi
 
-# Build the project using the determined number of cores
 cmake --build . -j $NUM_CORES
 
-# Run the compiled binary
 ./VulkanEngine
